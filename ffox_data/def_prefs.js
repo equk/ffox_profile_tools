4f2630165dfc4381ca15c2faa582c4645aaa69dc
equk <equk@users.noreply.github.com>
2025-10-25 11:49:22 +0100

    disable ai generated previews & suggestions on hovering links

--- ffox_data/def_prefs.js
+++ ffox_data/def_prefs.js
@@ -128,6 +128,7 @@ user_pref("browser.ml.chat.page", false);
 user_pref("browser.ml.chat.page.footerBadge", false);
 user_pref("browser.ml.chat.page.menuBadge", false);
 user_pref("browser.ml.chat.menu", false);
+user_pref("browser.ml.linkPreview.enabled", false);
 user_pref("browser.newtabpage.activity-stream.asrouter.providers.snippets", "{}");
 user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons", false);
 user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features", false);
